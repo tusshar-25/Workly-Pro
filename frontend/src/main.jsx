4923ac0ec537adae2458957828efe99f293ec89a
Tushar Rathod <tusharrathore853@gmail.com>
2025-11-12 00:10:29 +0530

    Updated for Deployement

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css"; // Tailwind CSS
 import "./styles/global.css";
 import { AuthProvider } from "./context/AuthContext";
 
